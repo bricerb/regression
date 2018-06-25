2373384606b103832488339f016cbb589d3f3e58
rblanch-incomm <rblanch@incomm.com>
2018-06-25 09:49:04 -0400

    fixed source and target version

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 # Resolve Mock Server Dependencies
-cp -r OTCRegression/data mock_server
-cd mock_server
-mvn dependency:resolve
-mvn spring-boot:run </dev/null &>/dev/null &
-cd ..
+# cp -r OTCRegression/data mock_server
+# cd mock_server
+# mvn dependency:resolve
+# mvn spring-boot:run </dev/null &>/dev/null &
+# cd ..
 
 # Resolve Regression Test Dependencies
 cd OTCRegression
