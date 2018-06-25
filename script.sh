c232e2406f7b1ddda5b52ca37f37546dcda1f3eb
rblanch-incomm <rblanch@incomm.com>
2018-06-25 10:09:31 -0400

    testing again

--- script.sh
+++ script.sh
@@ -1,16 +1,11 @@
 # Resolve Mock Server Dependencies
-# cp -r OTCRegression/data mock_server
-# cd mock_server
-# mvn dependency:resolve
-# mvn spring-boot:run </dev/null &>/dev/null &
-# cd ..
+cp -r OTCRegression/data mock_server
+cd mock_server
+mvn dependency:resolve
+mvn spring-boot:run </dev/null &>/dev/null &
+cd ..
 
 # Resolve Regression Test Dependencies
 cd OTCRegression
 mvn dependency:resolve
-appium </dev/null &>/dev/null &
-mvn test
-# cd ..
-
-# Start Spring-boot, appium, and regression test
-# cd mock_server & mvn spring-boot:run & cd ../OTCRegression & appium & mvn test
+appium & mvn test
